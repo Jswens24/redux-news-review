e38b1a4ea98bc7de1412a0caff389d7e72a6b708
Jessica Swenson <jessicamswens@gmail.com>
2023-03-14 07:23:22 -0600

    set up first slice

---
+++ src/redux/potentialArticleSlice.js
@@ -0,0 +1,26 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const potentialArticleSlice = createSlice({
+    name: 'potentialArticles',
+    initialState: {
+        value: [
+            {
+                title: 'This is the title of a news article'
+            }
+        ]
+    },
+    reducers: {
+        setPotentialArticles: (state, action) => {
+            state.value = action.payload
+        },
+        deletePotentialArticles: (state) => {
+            state.value = null
+        }
+    }
+})
+
+export const { setPotentialArticles, deletePotentialArticles } = potentialArticleSlice.actions;
+
+export const selectPotentials = (state) => state.setPotentialArticles.value;
+
+export default potentialArticleSlice.reducer;
