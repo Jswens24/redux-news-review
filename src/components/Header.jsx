35a3ea3145387f0a00642eec9c591e78929009fd
Jessica Swenson <jessicamswens@gmail.com>
2023-03-17 07:14:13 -0600

    added tailwindcss to project

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -23,7 +23,7 @@ const Header = () => {
 
 
     return (
-        <div>
+        <div className='flex justify-between '>
             <NavLink to='/'>Home</NavLink>
             <NavLink to='/details'>Details</NavLink>
 
